809927d5f6202ac0492ccd218fa66350661c352b
Dr. Asifur Rahman Akonjee <asifakonjee@hotmail.com>
2023-01-07 18:37:10 +0000

    Upload New File

---
+++ bin/autostart.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+## Copyright (C) 2020-2021 Aditya Shakya <adi1090x@gmail.com>
+## Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
+## Autostart Programs
+
+# Kill already running process
+_ps=(picom dunst ksuperkey xfce-polkit xfce4-power-manager)
+for _prs in "${_ps[@]}"; do
+	if [[ `pidof ${_prs}` ]]; then
+		killall -9 ${_prs}
+	fi
+done
+
+# Fix cursor
+xsetroot -cursor_name left_ptr
+
+# Polkit agent
+/usr/lib/xfce-polkit/xfce-polkit &
+
+# Enable power management
+xfce4-power-manager &
+
+# Enable Super Keys For Menu
+ksuperkey -e 'Super_L=Alt_L|F1' &
+ksuperkey -e 'Super_R=Alt_L|F1' &
+
+# Restore wallpaper
+hsetroot -cover ~/.config/i3/wallpapers/default.png
+
+# Lauch notification daemon
+~/.config/i3/bin/i3dunst.sh
+
+# Lauch polybar
+~/.config/i3/bin/i3bar.sh
+
+# Lauch compositor
+~/.config/i3/bin/i3comp.sh
+
+#Nitrogen
+nitrogen --restore &
+
+#Autotiling
+autotiling &
+
+# Ibus
+export GTK_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+export QT_IM_MODULE=ibus
+ibus-daemon -drxR
+
+#Octo-XBPS Notifier
+/usr/bin/octoxbps-notifier &
